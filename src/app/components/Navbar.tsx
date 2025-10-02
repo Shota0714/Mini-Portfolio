3ddb439cbe0207c29021cab2855a668a117441f9
Shota0714 <u.shota0714@gmail.com>
2025-10-02 12:11:10 -0400

    Created navbar and footer

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export default function Navbar() {
+    const pathname = usePathname();
+
+    const getLinkClass = (href: string) => {
+        if (pathname === href || pathname.startsWith(href + '/')) {
+            return 'activelink';
+        }
+        else {
+            return 'inactivelink';
+        }
+    }
+
+    return (
+        <nav>
+            <ul className="navLinks">
+                <li>
+                    <Link href="/" className={getLinkClass('/')}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/about" className={getLinkClass('/about')}>
+                        Bio
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/contact" className={getLinkClass('/contact')}>
+                        Contact
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/projects" className={getLinkClass('/projects')}>
+                        Projects
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/skills" className={getLinkClass('/skills')}>
+                        Skills
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -52,51 +53,18 @@ export default function Home() {
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <ul>
+          <li>
+            <Link href="https://www.linkedin.com/in/shota0714/" className="linkedinLink">
+              LinkedIn
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/Shota0714" className="githubLink">
+              Github
+            </Link>
+          </li>
+        </ul>
       </footer>
     </div>
   );
